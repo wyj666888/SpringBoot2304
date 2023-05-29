289160d332ad4b462981a57dbf63012df43328cd
wyj <2405086407@qq.com>
2023-05-29 09:33:09 +0800

    3

---
+++ src/test/java/com/tedu/springboot2304/SpringBoot2304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.springboot2304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
